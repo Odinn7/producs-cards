56ae86e705b397ced19150d41e2c77df95886014
Andrew <andreyod@wizardsdev.com>
2022-10-13 18:07:01 +0400

    add-currentFile-view favorite-products-adding

--- src/redux/actionCreators.tsx
+++ src/redux/actionCreators.tsx
@@ -14,13 +14,3 @@ export const productsFetchingAction = () =>  async (dispatch: AppDispatch) => {
         console.warn("FETCHING ERROR =>>>>",error)
     }
 }
-
-
-export const oneProductsFetchingAction = (id: number) =>  async (dispatch: AppDispatch) => {
-    try {
-        const response = await axios.get(`${baseURL}?id={id}`);
-        dispatch((response.data))
-    } catch (error) {
-        console.warn("FETCHING ERROR =>>>>",error)
-    }
-}
